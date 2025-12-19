84d15587327ddefe2a43da148ea3b77290ff4180
David <davidcorredormiguel25599@gmail.com>
2025-12-19 10:23:20 +0100

    About y Presentacion

--- tech_learn/src/pages/AboutUs.jsx
+++ tech_learn/src/pages/AboutUs.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import style from "../styles/AboutUs.module.css";
 
 function AboutUs() {
   return (
-    <>
-        <h1>Sobre Nosotros</h1>
-    </>
-  )
+    <div className={style.au}>
+      <img src="logo.png" alt="" className={style.logoau} />
+      <h1>Sobre Nosotros</h1>
+      <p>
+        Somos una tienda que reune en una sola todo tipo de tecnologías, desde
+        electrodomésticos, informática, sonido, hogar, y todo lo que puedas
+        necesitar.
+      </p>
+    </div>
+  );
 }
 
-export default AboutUs
+export default AboutUs;
