53fd05dfa496cb7bc595c7748462633c4fcc40b0
Iker Martínez Velasco <138805316+moimenta84@users.noreply.github.com>
2026-01-19 16:41:24 +0100

    productlist

--- tech_learn/src/components/ProductList.jsx
+++ tech_learn/src/components/ProductList.jsx
@@ -94,7 +94,7 @@ const ProductList = ({
   const totalPages = Math.ceil(filteredProducts.length / productsPerPage);
 
   return (
-    <div className="flex gap-6">
+    <div className="flex gap-6 ">
       {/* SIDEBAR FILTROS */}
       <aside className="w-64 flex-shrink-0">
         <div className="bg-white rounded-xl shadow-lg sticky top-4 border border-gray-100">
@@ -234,7 +234,7 @@ const ProductList = ({
       </aside>
 
       {/* CONTENIDO PRINCIPAL */}
-      <div className="flex-1 space-y-6">
+      <div className="flex-1 ">
         {/* GRID O LISTA */}
         {filteredProducts.length === 0 ? (
           <div className="bg-white rounded-lg shadow-md p-8 text-center">
@@ -313,26 +313,28 @@ const ProductList = ({
 
         {/* PAGINACIÓN */}
         {showPagination && filteredProducts.length > 0 && (
-          <div className="flex justify-center items-center gap-4">
-            <button
-              onClick={() => setCurrentPage(currentPage - 1)}
-              disabled={currentPage === 1}
-              className="px-4 py-2 bg-teal-600 text-white rounded disabled:opacity-50 
-                disabled:cursor-not-allowed hover:bg-teal-700 transition-colors"
-            >
-              ⬅ Anterior
-            </button>
-            <span className="font-semibold">
-              Página {currentPage} de {totalPages}
-            </span>
-            <button
-              onClick={() => setCurrentPage(currentPage + 1)}
-              disabled={indexOfLastProduct >= filteredProducts.length}
-              className="px-4 py-2 bg-teal-600 text-white rounded disabled:opacity-50 
-                disabled:cursor-not-allowed hover:bg-teal-700 transition-colors"
-            >
-              Siguiente ➡
-            </button>
+          <div className="w-full py-16">
+            <div className="flex justify-center items-center gap-4">
+              <button
+                onClick={() => setCurrentPage(currentPage - 1)}
+                disabled={currentPage === 1}
+                className="px-4 py-2 text-white rounded bg-orange-500 hover:bg-orange-600 disabled:opacity-50"
+              >
+                ⬅ Anterior
+              </button>
+
+              <span className="font-semibold">
+                Página {currentPage} de {totalPages}
+              </span>
+
+              <button
+                onClick={() => setCurrentPage(currentPage + 1)}
+                disabled={indexOfLastProduct >= filteredProducts.length}
+                className="px-4 py-2 text-white rounded bg-orange-500 hover:bg-orange-600 disabled:opacity-50"
+              >
+                Siguiente ➡
+              </button>
+            </div>
           </div>
         )}
 
